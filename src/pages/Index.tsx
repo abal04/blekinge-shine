a3e00cf2e1df80521abb756c81c978c02663c51f
Edvin Topalovic <55089202+edvintopa@users.noreply.github.com>
2026-01-09 21:10:10 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,6 +11,12 @@ const Index = () => {
   // Enable automatic dark mode based on system preference
   useTheme();
 
+  console.log({
+    service: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+    template: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+    publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+  });
+
   return (
     <div className="min-h-screen bg-background">
       <Header />
